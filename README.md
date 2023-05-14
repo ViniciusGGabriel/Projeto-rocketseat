07b0151a5c67f714ecc6007457baac0bdf571e9a
Vinícius Gabriel <132325593+viniciusggabriel@users.noreply.github.com>
2023-05-13 21:36:17 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Projeto-Rocketseat
+ Projeto feito no curso basico de HTML,CSS e JS
---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMode() {
+    const html = document.documentElement
+
+    html.classList.toggle('light')
+/* Esse comando substitui um codigo gigante de if e else e etc */
+}
